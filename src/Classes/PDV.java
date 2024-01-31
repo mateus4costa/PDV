6b84e026837f674cbec566244872082db5c3d926
Mateus Sampaio da Costa <60814658+mateus4costa@users.noreply.github.com>
2024-01-31 19:09:06 -0300

    Add files via upload

---
+++ src/Classes/PDV.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Classes;
+
+/**
+ *
+ * @author convidado
+ */
+public class PDV {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JanelaPrincipal janela = new JanelaPrincipal(null, true);
+        janela.setLocationRelativeTo(null);
+        janela.setVisible(true);
+    }
+    
+}
---
+++ src/Classes/Venda.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+import java.util.Date;
+
+
+/**
+ *
+ * @author convidado
+ */
+public class Venda {
+    private Date data;
+    private String hora;
+    private float valor_total;
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public float getValor_total() {
+        return valor_total;
+    }
+
+    public void setValor_total(float valor_total) {
+        this.valor_total = valor_total;
+    }
+
+    
+    
+}
